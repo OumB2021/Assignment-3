ad179b32ebe138a4c0b36bf293b21dd348a7135c
Oumb2021 <baarryoumar@gmail.com>
2023-04-24 13:18:15 -0400

    some test cases passed

--- Assignment3.py
+++ Assignment3.py
@@ -2,15 +2,20 @@ class Product:
 
   #constructor
   def __init__(self, name, price, stock):
-    self.name = name
-    self.price = price
-    self.stock = stock
+    if (stock < 0 ):
+      raise ValueError("insufficient stock")
+    else:
+      self.name = name
+      self.price = price
+      self.stock = stock
 
   def get_info(self):
-    return "Product's name: %s, price: %.2f, stock: %s" % (self.name, self.price, self.stock)
+    return "%s - price: $%.2f - stock: %s" % (self.name, self.price, self.stock)
 
   def sell (self, quantity):
-    if (self.stock < 0 or quantity > self.stock):
+    if (self.stock < 0 ):
+      raise ValueError("insufficient stock")
+    elif (quantity > self.stock):
       raise ValueError("insufficient quantity")
     else:
       self.stock -= quantity
@@ -25,7 +30,7 @@ class Electronics(Product):
       self.warranty = warranty
   
   def get_info(self):
-    return "Product's name: %s, price: %.2f, stock: %s, warranty: %s months" % (self.name, self.price, self.stock, self.warranty)
+    return "%s - Price: $%.2f - Stock: %s - Warranty: %s months" % (self.name, self.price, self.stock, self.warranty)
 
 class Clothing(Product):
 
@@ -37,16 +42,45 @@ class Clothing(Product):
       self.size = size
 
   def get_info(self):
-    return "Product's name: %s, price: %.2f, stock: %s, size: %s" % (self.name, self.price, self.stock, self.size)
+    return "%s -, Price: $%.2f - Stock: %s - size: %s" % (self.name, self.price, self.stock, self.size)
+
+class Store:
+
+  def __init__(self):
+    self.inventory = []
+
+  def add_product(self, product):
+    self.inventory.append(product)
+
+  def sell_product(self, product_name, quantity):
+
+    for item in self.inventory:
+      if (product_name.title() == item.__class__.__name__):
+        item.sell(quantity)
+        return item.price * quantity
+
+    raise ValueError("item was not found in the store")
+
+  # def get_inventory(self):
+
+  #   # for item in inventory:
+
+
 
 def main():
-  p = Product("p", 1.98, 3)
-  print(p.get_info())
+  p = Product("p", 1.98, 5)
 
   e = Electronics("p", 1.23, 4, 4)
-  print(e.get_info())
 
-  c = Clothing("c", 2.0, 4, "l")
-  print(c.get_info())
+  c = Clothing("c", 2.54, 3, "s")
+
+  s = Store()
+  s.add_product(c)
+  s.add_product(e)
+  s.add_product(p)
+  a = s.sell_product("product", 2)
+  print (a)
+
+
 
 main()
